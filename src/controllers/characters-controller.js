b81007c39fa60a6c82c77bfb6970f5ef07338aa5
klkode <katrinalkite@gmail.com>
2024-03-26 15:58:52 -0400

    Add route and controller for getting all of the character details for a specific character, as long as the user is verified and is the owner of that character.

--- src/controllers/characters-controller.js
+++ src/controllers/characters-controller.js
@@ -17,8 +17,85 @@ const getAllUsersCharacters = async (req, res) => {
         // Return the results of the query
         res.status(200).json(data);
     } catch (error) {
-        res.status(500).send(`Error retrieving users characters: ${error}`)
+        res.status(500).send(`Error retrieving users characters: ${error}`);
     }
 }
 
-module.exports = { getAllUsersCharacters };
+const getCharacterDetails = async (req, res) => {
+    try{
+        // TODO check if user_id exists in database?
+        const userId = req.decode.user_id;
+        // Check to see if the ID of the inventory is valid
+        const characterId = req.params.id;
+        if (!characterId || isNaN(characterId)) {
+            // Return the response of an invalid ID
+             return res.status(400).send(`Invalid character id: ${characterId}`);
+        }
+
+        //Get a list of the characters belonging to this user. Only return the information on the character's name, id, and their background's name 
+        const characterData = await knex('characters')
+        .select(
+            "characters.character_id",
+            "characters.name AS character_name",
+            "characters.gender",
+            "characters.age",
+            "characters.birthplace",
+            "characters.residence",
+            "characters.background_id",
+            "characters.special_people",
+            "characters.favoured_possession",
+            "characters.mania",
+            "characters.traits",
+            "characters.injuries",
+            "characters.feats",
+            "characters.spells",
+            "characters.notes"            
+        )
+        .where({user_id: userId, 'characters.character_id': characterId})
+        .first();
+
+        if(!characterData){
+            // Return the results of the query
+            res.status(404).json({error: `No character with id ${characterId} belongs to user with id ${userId}`});
+        }else{
+            // Get the character's stats as an object
+            const statsData = await knex('stats')
+            .select("*")          
+            .where({character_id: characterId})
+            .first();
+
+            // Remove the unnecessary keys from the object
+            delete statsData.character_id;
+            delete statsData.created_at;
+            delete statsData.updated_at;
+
+            // Get an array of all the character's skill points
+            const skillsData = await knex('skill_points')
+            .select(
+                "skill_points.skill_id",
+                "skill_points.points",
+            )          
+            .where({character_id: characterId});
+
+             // Get an array of all the character's equipment
+             const equipmentData = await knex('equipment')
+             .select(
+                 "equipment.equipment_id",
+                 "equipment.name",
+             )          
+             .where({character_id: characterId});
+            
+            // Add them onto the characterData object
+            const characterSheet = {...characterData, stats: statsData, skills: skillsData,  equipment: equipmentData};
+            
+            // Return the results of the query
+            res.status(200).json(characterSheet);
+        }
+
+
+    }catch(error){
+        res.status(500).send(`Error retrieving character's details: ${error}`);
+    }
+}
+
+module.exports = { getAllUsersCharacters, getCharacterDetails };
--- src/routes/characters-routes.js
+++ src/routes/characters-routes.js
@@ -8,4 +8,10 @@ router
     .route("/")
     .get(authorize, charactersController.getAllUsersCharacters);
 
+//Route for /characters/:id
+// GETs the details for a specified character that matched the id from characters table
+router
+    .route("/:id")
+    .get(authorize, charactersController.getCharacterDetails);
+
 module.exports = router;
