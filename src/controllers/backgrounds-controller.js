579996a0c61d6a09f8ce8a698574175ae77cbf8a
klkode <katrinalkite@gmail.com>
2024-03-27 22:43:26 -0400

    Update the GET backgrounds response to include an array of the background skill options.

--- src/controllers/backgrounds-controller.js
+++ src/controllers/backgrounds-controller.js
@@ -2,14 +2,30 @@ const knex = require("knex")(require("../../knexfile.js"));
 
 const getAllBackgrounds = async (req, res) => {
     try {
-        const data = await knex('backgrounds').select(
+        const backgroundData = await knex('backgrounds')
+        .select(
             "background_id",
             "name",
             "credit_rating_min",
             "credit_rating_max",
             "skill_point_calculation"
         );
-        res.status(200).json(data);
+        
+        // Append an arrary of the backgrounds occupational skill options to each background
+        for(let index = 0; index < backgroundData.length; index++){
+            const backgroundOptions = await knex('background_skills')
+            .select(
+                "option",
+                "is_choice",
+                "is_category",
+                "is_all",
+                "choices"
+            )
+            .where({background_id: backgroundData[index].background_id});
+            backgroundData[index].options = backgroundOptions;
+        }
+
+        res.status(200).json(backgroundData);
     } catch (error) {
         res.status(400).send(`Error retrieving backgrounds list: ${error}`)
     }
